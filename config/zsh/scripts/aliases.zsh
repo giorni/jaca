6a31a7670736bf89fe7c5458cb38eae2798fc27b
Ricardo Giorni <ricardo@giorni.co>
2018-07-21 15:10:16 -0300

    remove old repository dependencies at zsh config

--- config/zsh/scripts/aliases.zsh
+++ config/zsh/scripts/aliases.zsh
@@ -1,8 +1,5 @@
 # Aliases in this file are bash and zsh compatible
 
-# Don't change. The following determines where YADR is installed.
-yadr=$HOME/.yadr
-
 # Get operating system
 platform='unknown'
 unamestr=$(uname)
@@ -11,13 +8,6 @@ if [[ $unamestr == 'Linux' ]]; then
 elif [[ $unamestr == 'Darwin' ]]; then
   platform='darwin'
 fi
-
-# YADR support
-alias yav='yadr vim-add-plugin'
-alias ydv='yadr vim-delete-plugin'
-alias ylv='yadr vim-list-plugin'
-alias yup='yadr update-plugins'
-alias yip='yadr init-plugins'
 
 # PS
 alias psa="ps aux"
@@ -32,6 +22,23 @@ alias cls='clear;ls'
 alias df='df -h'
 alias du='du -h -d 2'
 
+# Common shell functions
+alias less='less -r'
+alias tf='tail -f'
+alias l='less'
+alias lh='ls -alt | head' # see the last modified files
+alias cl='clear'
+alias ka9='killall -9'
+alias k9='kill -9'
+
+# Finder
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+# Zippin
+alias gz='tar -zcvf'
+
+
 if [[ $platform == 'linux' ]]; then
   alias ll='ls -alh --color=auto'
   alias ls='ls --color=auto'
@@ -45,28 +52,15 @@ alias lsg='ll | grep'
 
 # Alias Editing
 TRAPHUP() {
-  source $yadr/zsh/aliases.zsh
+  source $XDG_CONFIG_HOME/zsh/scripts/aliases.zsh
 }
 
-alias ae='vim $yadr/zsh/aliases.zsh' #alias edit
-alias ar='source $yadr/zsh/aliases.zsh'  #alias reload
+alias ae='vim $XDG_CONFIG_HOME/zsh/scripts/aliases.zsh' #alias edit
+alias ar='source $XDG_CONFIG_HOME/zsh/scripts/aliases.zsh'  #alias reload
 alias gar="killall -HUP -u \"$USER\" zsh"  #global alias reload
-
-# vim using
-mvim --version > /dev/null 2>&1
-MACVIM_INSTALLED=$?
-if [ $MACVIM_INSTALLED -eq 0 ]; then
-  alias vim="mvim -v"
-fi
 
 # mimic vim functions
 alias :q='exit'
-
-# vimrc editing
-alias ve='vim ~/.vimrc'
-
-# zsh profile editing
-alias ze='vim ~/.zshrc'
 
 # Git Aliases
 alias gs='git status'
@@ -130,48 +124,26 @@ alias gt='git t'
 alias gbg='git bisect good'
 alias gbb='git bisect bad'
 alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+# Git tools
+alias hpr='hub pull-request'
+alias grb='git recent-branches'
 
-# Common shell functions
-alias less='less -r'
-alias tf='tail -f'
-alias l='less'
-alias lh='ls -alt | head' # see the last modified files
-alias screen='TERM=screen screen'
-alias cl='clear'
-
-# Zippin
-alias gz='tar -zcvf'
-
-# Ruby
-alias c='rails c' # Rails 3
+# Ruby/Rails
+alias c='rails c' # Rails 3+
 alias co='script/console' # Rails 2
-alias cod='script/console --debugger'
-
+# Gem install
+alias sgi='sudo gem install --no-ri --no-rdoc'
+# rake db
+alias rdm='rake db:migrate'
+alias rdmr='rake db:migrate:redo'
 #If you want your thin to listen on a port for local VM development
 #export VM_IP=10.0.0.1 <-- your vm ip
 alias ts='thin start -a ${VM_IP:-127.0.0.1}'
-alias ms='mongrel_rails start'
 alias tfdl='tail -f log/development.log'
 alias tftl='tail -f log/test.log'
-
-alias ka9='killall -9'
-alias k9='kill -9'
-
-# Gem install
-alias sgi='sudo gem install --no-ri --no-rdoc'
-
-# TODOS
-# This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
-# to find the note called 'todo'
-alias todo='open nvalt://find/todo'
-
 # Forward port 80 to 3000
 alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
-
-alias rdm='rake db:migrate'
-alias rdmr='rake db:migrate:redo'
-
-# Zeus
+# zeus pre-loader
 alias zs='zeus server'
 alias zc='zeus console'
 alias zr='zeus rspec'
@@ -180,8 +152,7 @@ alias zrs='zeus rails s'
 alias zrdbm='zeus rake db:migrate'
 alias zrdbtp='zeus rake db:test:prepare'
 alias zzz='rm .zeus.sock; pkill zeus; zeus start'
-
-# Rspec
+# rspec
 alias rs='rspec spec'
 alias sr='spring rspec'
 alias src='spring rails c'
@@ -189,20 +160,7 @@ alias srgm='spring rails g migration'
 alias srdm='spring rake db:migrate'
 alias srdt='spring rake db:migrate'
 alias srdmt='spring rake db:migrate db:test:prepare'
-
-
-# Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
-alias sp='sprintly'
-# spb = sprintly branch - create a branch automatically based on the bug you're working on
-alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
-
-alias hpr='hub pull-request'
-alias grb='git recent-branches'
-
-# Finder
-alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-
+# spring pre-loader
 alias dbtp='spring rake db:test:prepare'
 alias dbm='spring rake db:migrate'
 alias dbmr='spring rake db:migrate:redo'
@@ -210,9 +168,9 @@ alias dbmd='spring rake db:migrate:down'
 alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
-alias brewu='brew update  && brew upgrade && brew cleanup && brew prune && brew doctor'
+alias brewu='brew update && brew upgrade && brew cleanup && brew prune && brew doctor'
 
-alias npmOutdated='npm -g outdated --parseable=true | cut -d : -f 4 | xargs -n 1 npm -g install'
-
+# Node
 alias n=nodemon
 alias y=yarn
+alias npmOutdated='npm -g outdated --parseable=true | cut -d : -f 4 | xargs -n 1 npm -g install'
