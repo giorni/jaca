d106b56a9b94a1c7f1217bca94c2894e2c99b317
Ricardo Giorni <ricardo@giorni.co>
2026-03-06 17:54:14 -0600

    Remove ruby and validate rust
    If ruby version is not installed, or rust is not installed, don't try to
    load/source it. Validate that rust is installed. Leave ruby to .secrets

--- .config/zsh/scripts/ruby.zsh
+++ .config/zsh/scripts/ruby.zsh
@@ -1,8 +1,6 @@
 source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
 source /opt/homebrew/opt/chruby/share/chruby/auto.sh
 
-chruby ruby-3.4
-
 # NVim/Ruby LSP
 # https://github.com/castwide/solargraph
 SOLARGRAPH_CACHE=$XDG_CACHE_HOME/solargraph/
--- .config/zsh/scripts/rust.zsh
+++ .config/zsh/scripts/rust.zsh
@@ -2,4 +2,7 @@ export RUSTUP_HOME=$XDG_DATA_HOME/rustup
 
 # pathAppend $HOME/.cargo/bin
 
-. "$HOME/.cargo/env"
+if [[ -f "$HOME/.cargo/env" ]]; then
+  . "$HOME/.cargo/env"
+fi
+
